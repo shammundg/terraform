2c70118a2b44555d1c26d3d6fbc0e330825c117f
unknown <shammundg>
2025-10-31 18:07:37 +0530

    Add day-5-target-resources as normal folder

---
+++ day-5-target-resources/main.tf
@@ -0,0 +1,21 @@
+
+resource "aws_instance" "name" {
+    ami = "ami-0bdd88bd06d16ba03"
+     
+    instance_type = "t2.micro"
+    availability_zone = "us-east-1a"
+    tags = {
+        Name = "dev"
+    }
+
+}
+
+resource "aws_s3_bucket" "name" {
+    bucket = "shammuuuuu"
+  
+
+}
+
+
+#teragte resource we can user to apply specific resource level only belwo command is the reference 
+#terraform apply -target=aws_s3_bucket.name
